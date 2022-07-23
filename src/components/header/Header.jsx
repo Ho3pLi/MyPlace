7602e6b580856989dc74e7bfce0cf9d5fca3fc5b
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-23 23:02:33 +0200

    Header Component JSX

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -10,7 +12,15 @@ const Header = () => {
         <h5>Hi I'm</h5>
         <h1>Daniele Barile</h1>
         <h5 className="test-light">Fullstack Developer</h5>
+        <CTA />
 
+        <div className="me">
+
+          <img src={ME} alt="me" />
+
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
 
     </header>
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderSocial = () => {
+  return (
+    <div className="header__social">
+
+        <a href="https://www.linkedin.com/in/danielebarile/" target="_blank"></a>
+        <a href="https://github.com/Ho3pLi" target="_blank"></a>
+        <a href="https://gitlab.com/Ho3pLi" target="_blank"></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocial
