47bb0a20871ca100ef3d4fae65b971f8c49d27ce
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-28 17:20:57 +0200

    Services Component JSX

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
@@ -8,6 +9,139 @@ const Services = () => {
       <h5>What I Can Do</h5>
       <h6>at this point i think we understand that i develop code</h6>
       <h2>Services</h2>
+
+      <div className="container services__container">
+
+        <article className="service">
+
+          <div className="service__head">
+
+            <h3>UI/UX Design</h3>
+
+          </div>
+
+          <ul className='service__list'>
+
+            <li>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+            </li>
+
+          </ul>
+
+        </article> {/*first*/}
+
+        <article className="service">
+
+          <div className="service__head">
+
+            <h3>Web Development</h3>
+
+          </div>
+
+          <ul className='service__list'>
+
+            <li>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+            </li>
+
+          </ul>
+
+        </article> {/*second*/}
+
+        <article className="service">
+
+          <div className="service__head">
+
+            <h3>other stuff</h3>
+
+          </div>
+
+          <ul className='service__list'>
+
+            <li>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+              <BiCheck className='service__list-icon'/>
+              <p>
+                irdgas
+              </p>
+
+            </li>
+
+          </ul>
+
+        </article> {/*third*/}
+
+      </div>
 
     </section>
   )
