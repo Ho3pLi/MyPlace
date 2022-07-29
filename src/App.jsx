8e38aae2f637355abbc2ec9d3dd85704ddb12fe5
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-29 14:28:38 +0200

    Removed Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Blog from './components/blog/Blog'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -18,7 +17,6 @@ const App = () => {
       <Experience />
       <Services />
       <Blog />
-      <Testimonials />
       <Contact />
       <Footer />
     </>
