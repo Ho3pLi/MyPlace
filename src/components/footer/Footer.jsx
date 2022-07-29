7fa099094e1077696d6d9899015aa6825d7eda52
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-29 20:52:46 +0200

    Footer Section Done
    both JSX and CSS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './footer.css'
+import {FaGithub} from 'react-icons/fa'
+import {FiGitlab} from 'react-icons/fi'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <a href="#" className='footer__logo'>Daniele Barile</a>
+
+      <ul className='permalinks'>
+
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+
+      <div className="footer__socials">
+
+        <a href="https://github.com"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/danielebarile/"><BsLinkedin/></a>
+        <a href="https://gitlab.com"><FiGitlab/></a>
+
+      </div>
+
+      <div className="footer__copyright">
+
+        <small>&copy; Daniele Barile. All rights reserved.</small>
+
+      </div>
+
+    </footer>
   )
 }
 
