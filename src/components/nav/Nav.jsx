45dec4615e5553800cdbe04cdf1e377237419374
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-31 11:04:41 +0200

    Nav Modified
    I modified the home NavBar so that clicking on the book icon will redirect you to the Blog.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,8 +6,11 @@ import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const Nav = () => {
+
+  let navigate = useNavigate()
 
   const [activeNav, setActiveNav] = useState('#')
 
@@ -17,7 +20,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><RiServiceLine/></a>
-      <a href="#blog" onClick={() => setActiveNav('#blog')} className={activeNav === '#blog' ? 'active' : ''}><BiBook/></a>
+      <a href="/Blog" onClick={() => {navigate("./Blog"); setActiveNav('#blog')}} className={activeNav === '#blog' ? 'active' : ''}><BiBook/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
 
     </nav>
