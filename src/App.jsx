2b5458af0a2979f7b73d70a9f2f34c9ed8a0b96d
Ho3pLi <daniele.barile0101@gmail.com>
2022-07-31 11:06:52 +0200

    Blog Route
    I have created a new route.
    
    The 'Blog' route which, when called, renders the 'Blog' section and the 'NavG' section

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import NavG from './components/navG/NavG'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import BlogPreview from './components/blogPreview/BlogPreview'
+import Blog from './components/blog/Blog'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -13,20 +15,26 @@ const App = () => {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path='/' element={
-            <>
-              <Header />
-              <Nav />
-              <About />
-              <Experience />
-              <Services/>
-              <BlogPreview/>
-              <Contact/>
-              <Footer/>
-            </>}
-          />
-        </Routes>
+          <Routes>
+            <Route path='/' element={
+              <>
+                <Header />
+                <Nav />
+                <About />
+                <Experience />
+                <Services/>
+                <BlogPreview/>
+                <Contact/>
+                <Footer/>
+              </>}
+            />
+            <Route path='/Blog' element={
+              <>
+                <Blog/>
+                <NavG/>
+              </>}  
+            />
+          </Routes>
       </Router>
     </>
     )
