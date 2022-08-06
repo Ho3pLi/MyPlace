54027af6179582761b0405644eda9dcf12d86287
Ho3pLi <daniele.barile0101@gmail.com>
2022-08-06 18:49:47 +0200

    Section 'Error' done
    I created a very simple error page

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './error.css'
+
+
 
 function Error() {
   return (
-    <div>Error</div>
+    <div className="container">
+
+      <div className="copy-container center-xy">
+
+        <p>oops, 404 error, try something else lol</p>
+        <h6>(try to find the easter egg)</h6>
+
+        <br />
+
+        <h5>if you're looking for a hidden thing, it isn't here haha</h5>
+        <small>o forse è proprio questa, chissà</small>
+
+      </div>
+
+    </div>
+
   )
 }
 
