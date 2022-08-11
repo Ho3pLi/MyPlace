f4b5839a7eca4931bbb56ed12b17c91eaf82447e
su6c0d3r <daniele.barile0101@gmail.com>
2022-08-11 21:21:28 +0200

    First Post Image Changed
    First post image, title and link changed

--- src/components/blog/Data.jsx
+++ src/components/blog/Data.jsx
@@ -1,13 +1,13 @@
 import React from "react"
-import POST from "../../assets/blog-post.jpg"
+import POST1 from "../../assets/MyFirstPost.png"
 
 const Data = [
 
     {
       id: 1,
-      image: POST,
-      title: "This is my first post!",
-      github: "https://github.com",
+      image: POST1,
+      title: "My first post!",
+      github: "https://github.com/Ho3pLi/MyPlace",
       read: "/Blog/buymeanr6please"
     }
   
