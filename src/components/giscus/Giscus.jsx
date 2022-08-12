cd5df38404c01d51e9b7a2ba9bb2ae64bb838ab4
Ho3pLi <daniele.barile0101@gmail.com>
2022-08-12 15:45:05 +0200

    Giscus added
    I added Giscus in dependencies, I will use it to manage comments under posts.
    I have created a component of its own, which will be recalled in each post

---
+++ src/components/giscus/Giscus.jsx
@@ -0,0 +1,22 @@
+import Giscus from '@giscus/react';
+
+export default function MyApp() {
+  return (
+    <Giscus
+      id="comments"
+      repo="Ho3pLi/MyPlace"
+      repoId="R_kgDOHki-Dg"
+      category="Announcements"
+      categoryId="DIC_kwDOHki-Ds4CQxTd"
+      mapping="pathname"
+      strict="0"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="dark"
+      lang="en"
+      crossorigin="anonymous"
+    />
+  );
+}
