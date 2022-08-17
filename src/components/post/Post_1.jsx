2a08b44c2b4e4ff8ff3321137fdddfe47b911a64
Ho3pLi <daniele.barile0101@gmail.com>
2022-08-17 12:37:54 +0200

    Little fix

--- src/components/post/Post_1.jsx
+++ src/components/post/Post_1.jsx
@@ -29,13 +29,13 @@ const Post_1 = () => {
 
       <div className="post__content">
 
-        <p>Hi, welcome to my Blog, my name is Daniele Barile and I aspire to become an excellent Fullstack Developer. 
-        I am still "in the beginning", I have just 4 years of experience in application development.
-        I am interested in a little bit of everything, client development, server development, frontend and backend.
-        I like to improve myself and "experiment" with unknown technologies.
-        In this blog I will publish articles of all kinds, all (or almost) what goes through my head.
-        This site was my first experiment with ReactJS, very interesting, if you want to take a look at the code, you can find a button in the article preview that will bring you back to the repo.
-        That said, I think I've said it all for now.
+        <p>Hi, welcome to my blog. My name's Daniele Barile and I aspire to become an excellent Fullstack Developer. 
+        I'd say that I'm still "at the beginning" but actually I've got 4 years of experience in application development.
+        I'm interested almost in everything: client development, server development, frontend, and backend.
+        I like to improve myself and to play with unknown technologies.
+        In this blog I'll publish articles of any kind, all about (or almost) what wanders in my head.
+        This site was my first experiment with ReactJS (very interesting);  if you want to take a look at the code, you can find a button in the article preview that will bring you to the repo.
+        At this point, I think I've said it all for now.
         Happy coding :)
         </p>
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
   return (
     <section id="services">
 
-      <h5>What I Can Do</h5>
+      <h5>What Can I Do</h5>
       <h6>(at this point i think we understand that i develop code)</h6>
       <h2>Services</h2>
 
