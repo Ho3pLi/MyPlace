9711b31a69d36ab3a707e5688a52dca6017cbfff
Ho3pLi <daniele.barile@cons.amplifon.com>
2024-09-15 01:01:21 +0200

    eadcnwapi v2

--- netlify/functions/eadcnwapi.mjs
+++ netlify/functions/eadcnwapi.mjs
@@ -1,15 +1,13 @@
 const crypto = require('crypto');
 
-export default async (event, context) => {
-
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: aicokj' }),
-    };
+export default async (req, context) => {
+  
+  if (req.method !== 'GET') {
+    return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: aicokj' }));
   }
-
-  const { challengeCode } = event.challenge_code;
+  const reqUrl = req.url;
+  const url = new URL(reqUrl);
+  const challengeCode = url.searchParams.get('challenge_code');
 
   if (!challengeCode) {
     return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: awdubo' }));
@@ -23,7 +21,7 @@ export default async (event, context) => {
   hash.update(endpoint);
   const responseHash = hash.digest('hex');
 
-  return new Response(JSON.stringify({challengeResponse: responseHash}));
+  return new Response(JSON.stringify({challengeResponse: responseHash}), {headers: {'Content-Type': 'application/json'}});
 };
 
 export const config = {
