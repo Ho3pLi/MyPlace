f2966ed49707beffac98f146d0f683d4dea8f55c
Ho3pLi <daniele.barile@cons.amplifon.com>
2024-09-17 10:52:07 +0200

    eadcnwapi v4

--- netlify/functions/eadcnwapi.mjs
+++ netlify/functions/eadcnwapi.mjs
@@ -1,39 +1,47 @@
 const crypto = require('crypto');
 
 export default async (req, context) => {
-  console.log('START');
-  console.log('req method: ',req.method);
-  
-  if (req.method !== 'GET') {
-    return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: aicokj' }));
-  }
-  const reqUrl = req.url;
-  console.log('req url: ',reqUrl);
-  const url = new URL(reqUrl);
-  console.log('URL: ',url);
-  const challengeCode = url.searchParams.get('challenge_code');
-  console.log('challenge code: ',challengeCode);  
+  try {
+    console.log('START');
+    console.log('req method: ',req.method);
+    
+    if (req.method !== 'GET') {
+      return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: aicokj' }));
+    }
+    const reqUrl = req.url;
+    console.log('req url: ',reqUrl);
+    const url = new URL(reqUrl);
+    console.log('URL: ',url);
+    const challengeCode = url.searchParams.get('challenge_code');
+    console.log('challenge code: ',challengeCode);  
 
-  if (!challengeCode) {
-    return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: awdubo' }));
-  }
+    if (!challengeCode) {
+      return new Response(JSON.stringify({ message: 'Some error occurred. Check the env file for the understanding the error code. Error code: awdubo' }));
+    }
 
-  const verificationCode = Netlify.env.get('verificationCode');
-  console.log('verification code: ',verificationCode);
-  const endpoint = Netlify.env.get('endpoint');
-  console.log('endpoint: ',endpoint);
-  const hash = crypto.createHash('sha256');
-  console.log('hash: ',hash);
-  hash.update(challengeCode);
-  hash.update(verificationCode);
-  hash.update(endpoint);
-  console.log('HASH UPDATE DONE');
-  
-  const responseHash = hash.digest('hex');
-  console.log('response hash: ',responseHash);  
-  console.log('END');
-  
-  return new Response(JSON.stringify({challengeResponse: responseHash}), {headers: {'Content-Type': 'application/json'}});
+    const verificationCode = Netlify.env.get('verificationCode');
+    console.log('verification code: ',verificationCode);
+    const endpoint = Netlify.env.get('endpoint');
+    console.log('endpoint: ',endpoint);
+    const hash = crypto.createHash('sha256');
+    console.log('hash: ',hash);
+    hash.update(challengeCode);
+    hash.update(verificationCode);
+    hash.update(endpoint);
+    console.log('HASH UPDATE DONE');
+
+    const responseHash = hash.digest('hex');
+    console.log('response hash: ',responseHash);  
+    console.log('END');
+
+    return new Response(JSON.stringify({challengeResponse: responseHash}), {headers: {'Content-Type': 'application/json'}});
+  } catch (error) {
+    console.log({ error });
+    return {
+      statusCode: 500,
+      message: error
+    }
+  }
 };
 
 export const config = {
